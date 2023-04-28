c3cbba3e9d0bbf61a1a20253c4cd09444af22771
Lievn Menschaert <lievenmenschaert@gmail.com>
2023-04-28 15:21:48 +0200

    Update gloveIOT.ino

--- gloveIOT/gloveIOT.ino
+++ gloveIOT/gloveIOT.ino
@@ -15,33 +15,15 @@ const char* mqttPassword = "x7iNJWfycxrdEz51";
 WiFiClient espKlant;
 PubSubClient client(espKlant);
 
-// rechterhand
-const int first = 2;    // pink - orange
-const int second = 4;   // ring - green
-const int third = 16;   // middle - blue
-const int fourth = 17;  // index - brown
-
-//linkerhandk
-const int fifth = 12;
-const int sixth = 14;
-const int seventh = 27;
-const int eight = 26;
-const int nine = 25;
-
-// rechterhand duim niet - linkerhand wel
+// info: rechterhand duim niet - linkerhand wel
+// todo: define which pin is which finger!
+int pins[] = {2, 4, 16, 17, 12, 14, 27, 26, 25};
+int numPins = 9;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(first, OUTPUT);
-  pinMode(second, OUTPUT);
-  pinMode(third, OUTPUT);
-  pinMode(fourth, OUTPUT);
-  pinMode(fifth, OUTPUT);
-  pinMode(sixth, OUTPUT);
-  pinMode(seventh, OUTPUT);
-  pinMode(eight, OUTPUT);
-  pinMode(nine, OUTPUT);
-
+  for (int i = 0; i < numPins; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   
@@ -66,7 +48,7 @@ void setup() {
       Serial.println("WiFi connected");
       Serial.println("IP address: ");
       Serial.println(WiFi.localIP());
-      break;  // Exit the loop if a successful connection is made
+      break; // exit
     } else {
       Serial.println("");
       Serial.println("Connection failed");
@@ -90,59 +72,16 @@ void reconnect() {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(!client.connected()){
     reconnect();
   }
   client.loop();
-/*
-  digitalWrite(first, HIGH);
-  delay(300);
-  digitalWrite(first, LOW);
-  delay(200);
-
-  digitalWrite(second, HIGH);
-  delay(300);
-  digitalWrite(second, LOW);
-  delay(200);
-
-  digitalWrite(third, HIGH);
-  delay(300);
-  digitalWrite(third, LOW);
-  delay(200);
-
-  digitalWrite(fourth, HIGH);
-  delay(300);
-  digitalWrite(fourth, LOW);
-  delay(200);
-
-  digitalWrite(fifth, HIGH);
-  delay(300);
-  digitalWrite(fifth, LOW);
-  delay(200);
-
-  digitalWrite(sixth, HIGH);
-  delay(300);
-  digitalWrite(sixth, LOW);
-  delay(200);
-
-  digitalWrite(seventh, HIGH);
-  delay(300);
-  digitalWrite(seventh, LOW);
-  delay(200);
-
-  digitalWrite(eight, HIGH);
-  delay(300);
-  digitalWrite(eight, LOW);
-  delay(200);
-  */
 }
 
 void onMessage(const char* topic, byte* payload, unsigned int length) {
@@ -157,16 +96,16 @@ void onMessage(const char* topic, byte* payload, unsigned int length) {
   //Serial.println(message);
 
   if (message[0] == '1') {
-    digitalWrite(first, HIGH);
+    digitalWrite(pins[0], HIGH);
   } else {
-    digitalWrite(first, LOW);
+    digitalWrite(pins[0], LOW);
   }
 
   if (message[1] == '1') {
-    digitalWrite(second, HIGH);
+    digitalWrite(pins[1], HIGH);
   } else {
-    digitalWrite(second, LOW);
+    digitalWrite(pins[1], LOW);
   }
-  // more to follow
+  // more to follow when i know the pin-finger relation
 
 }
